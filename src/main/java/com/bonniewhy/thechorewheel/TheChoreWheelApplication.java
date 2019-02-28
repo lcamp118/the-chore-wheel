9d6c0437379175902a0afa72cdbbe65fe64df44d
bonniewhy <bonnieinpixels@gmail.com>
2019-02-28 12:53:29 -0600

    Setting up the initial files for my project

---
+++ src/main/java/com/bonniewhy/thechorewheel/TheChoreWheelApplication.java
@@ -0,0 +1,13 @@
+package com.bonniewhy.thechorewheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheChoreWheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheChoreWheelApplication.class, args);
+	}
+
+}
