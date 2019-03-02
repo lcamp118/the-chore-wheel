6e2068582c40f7a551ba0ef35868b31409b7e300
Bonnie Why <bonnieinpixels@gmail.com>
2019-03-01 21:09:09 -0600

    Started designing my database and outlining the classes to build it.

--- src/main/java/com/bonniewhy/thechorewheel/models/Rooms.java
+++ src/main/java/com/bonniewhy/thechorewheel/models/Rooms.java
@@ -1,4 +1,7 @@
 package com.bonniewhy.thechorewheel.models;
 
 public class Rooms {
+
+    // Should I make this an enum because it's going to be a fixed list of rooms to choose from in a checkbox form?
+
 }
--- src/main/java/com/bonniewhy/thechorewheel/models/Users.java
+++ src/main/java/com/bonniewhy/thechorewheel/models/Users.java
@@ -22,9 +22,58 @@ public class Users {
     @Size(min = 3, max = 50)
     private String password;
 
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String favoriteActivity;
+
     @OneToMany
     @JoinColumn(name = "room_id")
     private List<Rooms> rooms = new ArrayList<>();
 
+    @OneToMany
+    @JoinColumn(name = "task_id")
+    private List<Tasks> tasks = new ArrayList<>();
+
+    public Users(String username, String password, String favoriteActivity) {
+        this.username = username;
+        this.password = password;
+        this.favoriteActivity = favoriteActivity;
+    }
+
+    public Users() { }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFavoriteActivity() {
+        return favoriteActivity;
+    }
+
+    public List<Rooms> getRooms() {
+        return rooms;
+    }
+
+    public List<Tasks> getTasks() {
+        return tasks;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFavoriteActivity(String favoriteActivity) {
+        this.favoriteActivity = favoriteActivity;
+    }
 
 }
