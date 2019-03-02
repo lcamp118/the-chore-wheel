5c0225f0e8afb6b9a129669dc44c4604fe726875
Bonnie Why <bonnieinpixels@gmail.com>
2019-03-01 19:39:01 -0600

    Adding new files from setting up the projet on my Windows computer

--- src/main/java/com/bonniewhy/thechorewheel/models/Users.java
+++ src/main/java/com/bonniewhy/thechorewheel/models/Users.java
@@ -1,4 +1,30 @@
 package com.bonniewhy.thechorewheel.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
 public class Users {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String password;
+
+    @OneToMany
+    @JoinColumn(name = "room_id")
+    private List<Rooms> rooms = new ArrayList<>();
+
+
 }
